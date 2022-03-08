4120f0fa4a4779895bd13165aff1651ac64dbcc3
William Marques <junior.stuart34@gmail.com>
2022-03-08 19:03:06 -0400

    created project

---
+++ src/test/java/com/willianmarques/sbmongodb/SbmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willianmarques.sbmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
