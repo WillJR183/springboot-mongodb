07046ebc97036613bb5c0a4099a019cc5874a522
William Marques <junior.stuart34@gmail.com>
2022-03-09 12:37:09 -0400

    getting a post by id

---
+++ src/main/java/com/willianmarques/sbmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.willianmarques.sbmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.willianmarques.sbmongodb.domain.Post;
+import com.willianmarques.sbmongodb.repositories.PostRepository;
+import com.willianmarques.sbmongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById( String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+	}
+	
+}
