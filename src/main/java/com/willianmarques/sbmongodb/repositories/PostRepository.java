09e987eae188ac5e463553c01c641faf8e01511b
William Marques <junior.stuart34@gmail.com>
2022-03-09 14:12:54 -0400

    simple query with @Query

--- src/main/java/com/willianmarques/sbmongodb/repositories/PostRepository.java
+++ src/main/java/com/willianmarques/sbmongodb/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.willianmarques.sbmongodb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.willianmarques.sbmongodb.domain.Post;
@@ -11,5 +12,8 @@ import com.willianmarques.sbmongodb.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 }
