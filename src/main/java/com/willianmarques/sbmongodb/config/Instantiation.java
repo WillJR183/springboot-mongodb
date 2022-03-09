5396d890b7d3f45cf4659198ed1a6fdaca8ce085
William Marques <junior.stuart34@gmail.com>
2022-03-08 22:55:23 -0400

    data projection from the author with DTO

--- src/main/java/com/willianmarques/sbmongodb/config/Instantiation.java
+++ src/main/java/com/willianmarques/sbmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.willianmarques.sbmongodb.domain.Post;
 import com.willianmarques.sbmongodb.domain.User;
+import com.willianmarques.sbmongodb.dto.AuthorDTO;
 import com.willianmarques.sbmongodb.repositories.PostRepository;
 import com.willianmarques.sbmongodb.repositories.UserRepository;
 
@@ -34,11 +35,11 @@ public class Instantiation implements CommandLineRunner {
 		User u2 = new User(null, "Luis", "luis@gmail.com");
 		User u3 = new User(null, "Guilherme", "gui@gmail.com");
 		
-		
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", u1);
-		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
+		
 		postRepository.saveAll(Arrays.asList(p1, p2));
 	}
 
