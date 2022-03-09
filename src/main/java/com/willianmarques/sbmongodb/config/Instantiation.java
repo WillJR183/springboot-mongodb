71737f109641699c2d31f6a470a1d044d3d3b78e
William Marques <junior.stuart34@gmail.com>
2022-03-08 22:43:27 -0400

    creating post entity with nested user

--- src/main/java/com/willianmarques/sbmongodb/config/Instantiation.java
+++ src/main/java/com/willianmarques/sbmongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.willianmarques.sbmongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.willianmarques.sbmongodb.domain.Post;
 import com.willianmarques.sbmongodb.domain.User;
+import com.willianmarques.sbmongodb.repositories.PostRepository;
 import com.willianmarques.sbmongodb.repositories.UserRepository;
 
 @Configuration
@@ -14,14 +18,28 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
-
+	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
+		
 		User u1 = new User(null, "Will", "will@gmail.com");
 		User u2 = new User(null, "Luis", "luis@gmail.com");
 		User u3 = new User(null, "Guilherme", "gui@gmail.com");
+		
+		
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", u1);
+		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		postRepository.saveAll(Arrays.asList(p1, p2));
 	}
 
 }
---
+++ src/main/java/com/willianmarques/sbmongodb/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.willianmarques.sbmongodb.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.willianmarques.sbmongodb.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {}
