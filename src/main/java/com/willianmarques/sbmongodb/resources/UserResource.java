84faefc77f9efde9d75b9bac1bf382029ba0186f
William Marques <junior.stuart34@gmail.com>
2022-03-08 21:24:47 -0400

    user insertion with POST

--- src/main/java/com/willianmarques/sbmongodb/resources/UserResource.java
+++ src/main/java/com/willianmarques/sbmongodb/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.willianmarques.sbmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.willianmarques.sbmongodb.domain.User;
 import com.willianmarques.sbmongodb.dto.UserDTO;
@@ -20,6 +23,14 @@ public class UserResource {
 	
 	@Autowired
 	private UserService userService;
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
+		User user = userService.fromDTO(objDTO);
+		user = userService.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
--- src/main/java/com/willianmarques/sbmongodb/services/UserService.java
+++ src/main/java/com/willianmarques/sbmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.willianmarques.sbmongodb.domain.User;
+import com.willianmarques.sbmongodb.dto.UserDTO;
 import com.willianmarques.sbmongodb.repositories.UserRepository;
 import com.willianmarques.sbmongodb.services.exceptions.ObjectNotFoundException;
 
@@ -15,6 +16,14 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
